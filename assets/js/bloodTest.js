447852ccb53beea0f4f73669a9b5e7c610377eec
sabarna basak <sabarnabasak@gmail.com>
2025-07-19 23:09:25 +0530

    implement update blood test

--- assets/js/bloodTest.js
+++ assets/js/bloodTest.js
@@ -65,6 +65,75 @@ $("#new-blood-test-form").submit((event) => {
     successHandler
   );
 });
+// edit blood test
+$("#edit-blood-test-form").submit((event) => {
+  event.preventDefault();
+  const form = $("#edit-blood-test-form");
+  const formData = form.serialize();
+  const submitBtn = $("#updateBloodTestDetails");
+  const submitUrl = "update-blood-test-entry";
+  const alertTextComponent = $("#custom-alert");
+  const beforeSendButton =
+    '<i class="fa fa-spinner fa-pulse"></i> Processing...';
+  const defaultBtnText = "Update";
+  const type = "POST";
+
+  const beforeSendHandler = () => {
+    submitBtn.attr("disabled", true).html(beforeSendButton);
+  };
+  const errorHandler = (xhr) => {
+    submitBtn.attr("disabled", false).html(defaultBtnText);
+  };
+
+  // Todo: Persisting previous state values while launching the modal
+
+  const successHandler = (response) => {
+    const jsonResponse = JSON.parse(response);
+    const { status, message } = jsonResponse;
+    if (status == "success") {
+      alertTextComponent
+        .removeClass("hide")
+        .removeClass("alert-danger")
+        .addClass("show")
+        .addClass("alert-success")
+        .html(`<i class="fa fa-check"></i> ${message}`);
+
+      submitBtn
+        .removeClass("btn-success")
+        .addClass("btn-primary")
+        .html(defaultBtnText);
+
+      $("#editBloodTest").modal("hide");
+      loadBloodTestList();
+    }
+    if (status === "error") {
+      alertTextComponent
+        .removeClass("hide")
+        .addClass("show")
+        .html(`<i class="fa-solid fa-triangle-exclamation"></i> ${message}`);
+      submitBtn.attr("disabled", false).html(defaultBtnText);
+    }
+    form[0].reset();
+    isCreated = true;
+    setTimeout(() => {
+      alertTextComponent
+        .addClass("hide")
+        .addClass("alert-danger")
+        .removeClass("show")
+        .removeClass("alert-success")
+        .html("");
+    }, 5000);
+  };
+
+  ajaxHandler(
+    type,
+    submitUrl,
+    formData,
+    beforeSendHandler,
+    errorHandler,
+    successHandler
+  );
+});
 
 // reload
 $("#close-btn").on("click", () => {
@@ -75,10 +144,14 @@ $("#close-btn").on("click", () => {
   isCreated = false;
 });
 
-// load data
-$(document).ready(() => {
+const loadBloodTestList = () => {
   const loader = $("#loader > div");
   const tableBody = $("#blood-test-table > tbody");
   const submitUrl = "get-blood-tests-list";
   loadTableContent(loader, tableBody, submitUrl);
+};
+
+// load data
+$(document).ready(() => {
+  loadBloodTestList();
 });
